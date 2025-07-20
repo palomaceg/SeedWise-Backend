988869811bbce5adf3a1701da5b5284a8d301136
talberts <trinidad.albert98@gmail.com>
2025-07-20 16:03:52 +0200

    cambio models mentoring

--- models/mentoring.js
+++ models/mentoring.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const MentoringSchema = new mongoose.Schema (
     {
-        name:{
+        category:{
             type: String,
             required: [true, 'Porfavor, indicanos tu nombre'],
         },
@@ -10,37 +10,14 @@ const MentoringSchema = new mongoose.Schema (
             type: String,
             required: [true, 'Por favor, indícanos el nombre de tu empresa'],
         },
-        position: {
-            type: String,
-            required: [true, 'Por favor, indicanos tu puesto de trabajo']
-        },
-        email: {
-            type: String,
-            match: [/^.*@.*\..*/, 'Este correo no es válido'],
-            required: [true, 'Por favor, rellena tu email'],
-        },
-        availabilityDate: {
-            type: Date,
-            required: [true, 'Por favor, indícanos tu fecha de disponibilidad'],
-        },
-        linkedin: {
-            type: String,
-        },
         website: {
             type: String,
         },
-        mentoringAreas: {
-            type: String,
-            required: [true, 'Por favor indícanos tus áreas de mentor']
+        mentor: {
+            type: Object,
         },
         logo: {
             type: String,
-        },
-        phone: {
-            type: String,
-        },
-        managementLink: {
-            type: String
         },
          },
         {
